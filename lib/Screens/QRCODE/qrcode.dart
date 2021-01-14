372226d3c506de740ce582be74efd30ba6c730de
DanielPetroni <daniel.petroni25@gmail.com>
2021-01-14 16:53:59 -0300

    Alterando design
    Alteração na cor de fundo, alteração no estilo do botão na tela de exibição do QrCode.

--- lib/Screens/QRCODE/qrcode.dart
+++ lib/Screens/QRCODE/qrcode.dart
@@ -13,7 +13,6 @@ class QrCodeOne extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: kPrimaryColor,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -27,42 +26,56 @@ class QrCodeOne extends StatelessWidget {
               ),
             ),
             SizedBox(height: size.height * 0.05),
-            RaisedButton(
-                child: Text('Usar Qr Code'),
-                onPressed: () {
-                  QrController().use(
-                      id: qr.sId,
-                      onSucess: (value) {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                  title: Column(
-                                    children: [
-                                      SvgPicture.asset(
-                                        "assets/images/confirmado.svg",
-                                        height: size.height * .2,
+            GestureDetector(
+                child: Container(
+                    margin: EdgeInsets.only(top: 20, bottom: 5),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    width: size.width * 0.8,
+                    decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      'Realizar pagamento!',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                      textAlign: TextAlign.center,
+                    )),
+                onTap: () => QrController().use(
+                    id: qr.sId,
+                    onSucess: (value) {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                                title: Column(
+                                  children: [
+                                    SvgPicture.asset(
+                                      "assets/images/confirmado.svg",
+                                      height: size.height * .2,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      '${value['message']}',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                                actions: <Widget>[
+                                  FlatButton(
+                                      child: Text(
+                                        "Fechar",
+                                        style: TextStyle(color: kPrimaryColor),
                                       ),
-                                      SizedBox(height: 10),
-                                      Text('${value['message']}', textAlign: TextAlign.center,),
-                                    ],
-                                  ),
-                                  actions: <Widget>[
-                                    FlatButton(
-                                        child: Text(
-                                          "Fechar",
-                                          style:
-                                              TextStyle(color: kPrimaryColor),
-                                        ),
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                          Navigator.pop(context);
-                                          Navigator.pop(context);
-                                        })
-                                  ]);
-                            });
-                      });
-                })
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                        Navigator.pop(context);
+                                        Navigator.pop(context);
+                                      })
+                                ]);
+                          });
+                    })),
           ],
         ),
       ),
